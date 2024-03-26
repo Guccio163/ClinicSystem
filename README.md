a9b1ca69d80082e450b21c82565f93f584b4e246
Wiktor Gut <411761@student.agh.edu.pl>
2024-03-26 22:09:04 +0100

    .env setup

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 to succesfully run the server: <br/>
 ~ cd back <br/>
+~ pip install python-dotenv <br/>
 ~ pip3 install fastapi <br/>
 ~ pip3 install uvicorn <br/>
 ~ uvicorn server:app --reload <br/>
--- back/server.py
+++ back/server.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from dotenv import dotenv_values
 
+config = dotenv_values("../.env")
 app = FastAPI()
+firebasePass = config.get("PASS")
 
 @app.get("/patients")
 async def getPatients():
-    return ["Patient1", "Patient2"]
+    return ["Patient1", "Patient2", firebasePass]
