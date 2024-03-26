28eadb1a3e4973fd5c550b6276de400d0d77b773
Wiktor Gut <411761@student.agh.edu.pl>
2024-03-26 23:19:18 +0100

    firebase config

--- back/server.py
+++ back/server.py
@@ -1,5 +1,47 @@
 from fastapi import FastAPI
 from dotenv import dotenv_values
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("../.firebaseCred.json")
+# firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://assecoclinic-default-rtdb.firebaseio.com'
+})
+ref = db.reference("/")
+print(ref)
+ref.get("/")
+data = {
+	"Book1":
+	{
+		"Title": "The Fellowship of the Ring",
+		"Author": "J.R.R. Tolkien",
+		"Genre": "Epic fantasy",
+		"Price": 100
+	},
+	"Book2":
+	{
+		"Title": "The Two Towers",
+		"Author": "J.R.R. Tolkien",
+		"Genre": "Epic fantasy",
+		"Price": 100	
+	},
+	"Book3":
+	{
+		"Title": "The Return of the King",
+		"Author": "J.R.R. Tolkien",
+		"Genre": "Epic fantasy",
+		"Price": 100
+	},
+	"Book4":
+	{
+		"Title": "Brida",
+		"Author": "Paulo Coelho",
+		"Genre": "Fiction",
+		"Price": 100
+	}
+}
 
 config = dotenv_values("../.env")
 app = FastAPI()
@@ -7,4 +49,6 @@ firebasePass = config.get("PASS")
 
 @app.get("/patients")
 async def getPatients():
-    return ["Patient1", "Patient2", firebasePass]
+    ref.set(data)
+    print(ref.get())
+    return data
